be825d6033f5a129c1bbc2b24e2a646d414e3e75
Ritesh jha <jhar27018@gmail.com>
2025-02-11 16:20:33 +0530

    Replace <img> tags with <Image> component for optimized image handling across multiple components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,17 @@
-
-
 import { socialMedia } from "../lib/utils";
-
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
       {/* background grid */}
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
+        <Image
           src="/footer-grid.svg"
           alt="grid"
-          className="w-full h-full opacity-50 "
+          className="w-full h-full opacity-50"
+          height={500}
+          width={500}
         />
       </div>
 
@@ -25,19 +25,17 @@ const Footer = () => {
           achieve your goals.
         </p>
         <a href="mailto:jainneel497@gmail.com">
-          <button>Let's get in touch</button>
+          <button>{"Let's get in touch"}</button>
         </a>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-center items-center">
-        
-
         <div className="flex items-center justify-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-            <div 
+            <div
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <img  src={info.img} alt="icons" width={20} height={20} />
+              <Image src={info.img} alt="icons" width={20} height={20} />
             </div>
           ))}
         </div>
